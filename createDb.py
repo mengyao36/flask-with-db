832d017b09fd9c9393a78291d74dad9296167dde
mengyao36 <mengyao.hu@stonybrook.edu>
2022-09-24 13:09:58 -0400

    more updates

--- createDb.py
+++ createDb.py
@@ -28,10 +28,10 @@ connect.commit()
 
 ## insert data into the table
 db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('12345', 'John', 'Smith', '01/01/2000', 'male', '01/01/2022', '01/02/2022', 'Medicare')")
-db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('23456', 'Jane', 'Doe', '02/02/2001', 'fmale', '02/01/2022', '02/02/2022', 'Medicare')")
-db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('34567', 'Mary', 'Smith', '03/03/2002', 'fmale', '03/01/2022', '03/02/2022', 'Medicaid')")
+db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('23456', 'Jane', 'Doe', '02/02/2001', 'female', '02/01/2022', '02/02/2022', 'Medicare')")
+db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('34567', 'Mary', 'Smith', '03/03/2002', 'female', '03/01/2022', '03/02/2022', 'Medicaid')")
 db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('45678', 'Bob', 'Smith', '04/04/2003', 'male', '04/01/2022', '04/02/2022', 'Medicare')")
-db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('56789', 'Jane', 'Doe', '05/05/2004', 'fmale', '05/01/2022', '05/02/2022', 'Private')")
+db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob, gender, admissionday, dischargeday, insurance) values('56789', 'Jane', 'Doe', '05/05/2004', 'female', '05/01/2022', '05/02/2022', 'Private')")
 
 connect.commit()
 
